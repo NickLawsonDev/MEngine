eb855879dbb4ebd43650ada985ebf010d3555dfb
Nick <buckley183@gmail.com>
2016-12-25 20:21:36 -0500

    initial

---
+++ MEngine.Core/src/main/main.cs
@@ -0,0 +1,110 @@
+﻿using MEngine.Core.src.utils;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MEngine.Core
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont counter;
+        FrameCounter _frameCounter = new FrameCounter();
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            this.Window.Title = Constants.TITLE;
+            this.Window.AllowUserResizing = Constants.ALLOWUSERRESIZING;
+            this.Window.IsBorderless = Constants.ISBORDERLESS;
+            graphics.PreferredBackBufferHeight = Constants.HEIGHT;
+            graphics.PreferredBackBufferWidth = Constants.WIDTH;
+            graphics.SynchronizeWithVerticalRetrace = Constants.VSYNC;
+            this.IsFixedTimeStep = Constants.ISFIXEDTIMESTEP;
+
+
+            graphics.ApplyChanges();
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            counter = Content.Load<SpriteFont>("fpscounter");
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            _frameCounter.Update(deltaTime);
+
+            var fps = string.Format("FPS: {0}", _frameCounter.AverageFramesPerSecond);
+
+
+            // TODO: Add your drawing code here
+            spriteBatch.DrawString(counter, fps, new Vector2(50,50), new Color(255, 104, 23, 1));
+
+            var pixel = new Texture2D(graphics, 20, 20);
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MEngine.Core/src/utils/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEngine.Core
+{
+    public static class Constants
+    {
+        //Window Properties
+        public static readonly int HEIGHT = 600;
+        public static readonly int WIDTH = 800;
+        public static readonly string TITLE = "MEngine";
+        public static readonly bool ALLOWUSERRESIZING = true;
+        public static readonly bool ISBORDERLESS = false;
+        public static readonly bool VSYNC = false;
+
+        //Game Properties
+        public static readonly bool ISDEBUG = true;
+        public static readonly bool ISFIXEDTIMESTEP = false;
+    }
+}
